a56f2635264998ddbcaf1d1c011f512e64ff34a3
Hayley Guillou <hayleyguillou@gmail.com>
2015-09-24 12:56:46 +0900

    Updated raw euler43

--- euler.py
+++ euler.py
@@ -262,6 +262,25 @@ def euler40():
     print(int(num[1]) * int(num[10]) * int(num[100]) * int(num[1000]) * int(num[10000]) * int(num[100000]) * int(num[1000000]))
     
     
+def euler43():
+    from itertools import permutations
+    
+    def is_divisible(p):
+    	divisors = [2,3,5,7,11,13,17]
+    	for i in range(0, len(divisors)):
+    		num = 100 * int(p[i+1]) + 10 * int(p[i+2]) + int(p[i+3])
+    		if num % divisors[i] != 0:
+    			return False
+    	return True
+    
+    def eul43():
+    	"""Find the sum of all 0 to 9 pandigital numbers with this property (See online)"""
+    	init = [''.join(p) for p in permutations('0123456789') if p[0]!='0' and p[5]=='5' and int(p[3])%2==0]
+    	return sum([int(p) for p in init if is_divisible(p)])
+    
+    print(eul43())
+    # 16695334890
+    
 def euler44():
     P = [ ( ( p * (3 * p - 1 ) ) / 2 ) for p in range(1, 3000) ]
 
@@ -427,22 +446,3 @@ def euler112():
         prop = numBouncy/curr
 
     print(curr, prop)
-    
-    
-def euler43():
-    from itertools import permutations
-    def isGood(p):
-    	if p[5] != '5' and p[5] != '0': 
-    		return False
-    	elif int(p[3]) % 2 != 0: 
-    		return False
-    	elif sum([int(d) for d in p[2:5]]) % 3 != 0: 
-    		return False
-    	elif int(p[4:7]) % 7 != 0: return False
-    	#if int(p[5:8]) % 11 != 0: return False
-    	else: 
-    		return True
-    	
-    
-    perms = [p for p in list(permutations('1234567890')) if isGood(p)]
-    print(len(perms))
